ceaa4a30ae292cc82611121947ce1194e548936f
aShinde <ananta.shinde@honeywellcom>
2023-10-25 09:00:57 +0530

    25/10/23

---
+++ C++/strams.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num = 20;
+    cin >> num ;
+    cout <<  "Value = " << num << endl;
+    cout <<  "Value = " << num;
+    return 0;
+}
